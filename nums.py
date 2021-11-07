eed06ac572cc0c0ada12dfa470d5364ab7831e46
Itarid <itarid@gmail.com>
2021-11-06 23:40:30 -0400

    main/py added and nums.py modified

--- nums.py
+++ nums.py
@@ -15,3 +15,6 @@ if __name__ == "__main__":
   if len(sys.argv) < 3:
     print "usage: ./indexer_update.py UPDATE_TSV_FILE_PATH TARGET_COLUMN_NUM"
     sys.exit(1)
+
+ def get_content_num(self):
+    return len(self.contentTable)
