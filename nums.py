3324d0fe113e92a8e3a20644863a23b3d8ca5663
Itarid <itarid@gmail.com>
2021-11-06 23:31:21 -0400

    Loop For added

--- nums.py
+++ nums.py
@@ -3,6 +3,14 @@ def main(filepath, column):
   f = codecs.open(filepath, "r", "utf-8")
   lines = f.readlines()
 
+  indexer.load ("./")
+
+  for line in lines:
+    print
+    line
+    elems = line.split ("\t")
+    indexer.append (''.join (elems[column - 1]))
+
 if __name__ == "__main__":
   if len(sys.argv) < 3:
     print "usage: ./indexer_update.py UPDATE_TSV_FILE_PATH TARGET_COLUMN_NUM"
